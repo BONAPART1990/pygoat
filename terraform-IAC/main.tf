4a93d8ed9d5381e5c2b90e0f02de7746d55c668b
BONAPART1990 <46047842+BONAPART1990@users.noreply.github.com>
2025-07-14 15:40:23 -0400

    Update main.tf

--- terraform-IAC/main.tf
+++ terraform-IAC/main.tf
@@ -35,3 +35,18 @@ module "avm-res-web-serverfarm" {
     name                = module.naming.app_service.name
     service_plan_resource_id = module.avm-res-web-serverfarm.resource_id
 }
+
+
+
+
+resource "azurerm_key_vault" "name" {
+  name = "shahbaz8768796"
+  location = "West US2"
+  resource_group_name = "shahbazdemo"
+  tenant_id       = "104e77d4-81e7-4c16-ab44-935220bed6dd"
+  sku_name = "standard"
+
+  purge_protection_enabled = false
+  soft_delete_retention_days = null
+
+}
